1bb882a810221cc284f6a99238d56cdfff8e3640
Rebecca Davidsson <rebecca.davidsson@vattenfall.com>
2024-09-24 19:14:23 +0200

    Update React 17

--- src/components/sections/FeaturesSplitWork.js
+++ src/components/sections/FeaturesSplitWork.js
@@ -66,20 +66,57 @@ const FeaturesSplitWork = ({
         <div className={innerClasses}>
           <SectionHeader data={sectionHeader} className="center-content" />
           <div className={splitClasses}>
+
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left"
+                   data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Vattenfall - September 2022 until present
+                  Triodos - August 2024 until present
                 </div>
                 <div className="text-xs fw-600 tt-u mb-8">
                   NAVARA
                 </div>
                 <h3 className="mt-0 mb-12">
-                  Web Development Vattenfall District Heating
+                  Web Development at Triodos Internet Banking
                 </h3>
                 <p className="m-0">
-                  Vattenfall is a European energy company and strives to make fossil-free living possible within one generation.
-                  With district heating, waste heat is used that is released when generating electricity or incinerating waste.
+                  Triodos Bank is a globally recognised leader in impact investing, offering sustainable investment funds across a broad range of sectors.
+                </p>
+              </div>
+              <div className={
+                classNames(
+                    'split-item-image center-content-mobile reveal-from-bottom has-shadow',
+                    imageFill && 'split-item-image-fill'
+                )}
+                   data-reveal-container=".split-item">
+                <a href={"https://www.triodos.nl/"} target={"_blank"} rel={"noreferrer noopener"}>
+                  <Image
+                      src={require('./../../assets/images/triodos.png')}
+                      alt="Triodos"
+                      width={528}
+                      height={396}/>
+                </a>
+              </div>
+            </div>
+            <Tiles data={tilesData.tiles.triodos}/>
+
+            <div className="split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left"
+                   data-reveal-container=".split-item">
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Vattenfall - September 2022 until July 2024
+                </div>
+                <div className="text-xs fw-600 tt-u mb-8">
+                  NAVARA
+                </div>
+                <h3 className="mt-0 mb-12">
+                  Web Development at Vattenfall
+                </h3>
+                <p className="m-0">
+                  Vattenfall is a European energy company and strives to make fossil-free living possible within one
+                  generation.
+                  With district heating, waste heat is used that is released when generating electricity or incinerating
+                  waste.
                 </p>
               </div>
               <div className={
@@ -89,47 +126,48 @@ const FeaturesSplitWork = ({
                 )}
                    data-reveal-container=".split-item">
                 <a href={"https://www.vattenfall.nl/"} target={"_blank"} rel={"noreferrer noopener"}>
-                <Image
-                    src={require('./../../assets/images/vattenfall.png')}
-                    alt="Vattenfall"
-                    width={528}
-                    height={396} />
+                  <Image
+                      src={require('./../../assets/images/vattenfall.png')}
+                      alt="Vattenfall"
+                      width={528}
+                      height={396}/>
                 </a>
               </div>
             </div>
-
-
             <Tiles data={tilesData.tiles.vattenfall}/>
 
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
+              <div className="split-item-content center-content-mobile reveal-from-left"
+                   data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   PWN - September 2021 until September 2022
-                  </div>
+                </div>
                 <div className="text-xs fw-600 tt-u mb-8">
                   NAVARA
                 </div>
                 <h3 className="mt-0 mb-12">
                   Migration to headless CMS
-                  </h3>
+                </h3>
                 <p className="m-0">
-                  At PWN (Provinciaal Waterleidingbedrijf Noord-Holland), I have been working on modernizing the entire website by
+                  At PWN (Provinciaal Waterleidingbedrijf Noord-Holland), I have been working on modernizing the entire
+                  website by
                   building a headless structure with a combination of Angular, .NET and Drupal.
-                  This project also gave me the opportunity to learn more about Azure DevOps CI/CD pipelines, web performance and SEO.
-                  </p>
+                  This project also gave me the opportunity to learn more about Azure DevOps CI/CD pipelines, web
+                  performance and SEO.
+                </p>
               </div>
               <div className={
                 classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom has-shadow',
-                  imageFill && 'split-item-image-fill'
+                    'split-item-image center-content-mobile reveal-from-bottom has-shadow',
+                    imageFill && 'split-item-image-fill'
                 )}
-                data-reveal-container=".split-item">
+                   data-reveal-container=".split-item">
                 <a href={"https://www.pwn.nl/"} target={"_blank"} rel={"noreferrer noopener"}>
-                <Image
-                  src={require('./../../assets/images/features-split-image-01.png')}
-                  alt="PWN"
-                  width={528}
-                  height={396} />
+                  <Image
+                      src={require('./../../assets/images/features-split-image-01.png')}
+                      alt="PWN"
+                      width={528}
+                      height={396}/>
                 </a>
               </div>
             </div>
@@ -137,15 +175,17 @@ const FeaturesSplitWork = ({
             <Tiles data={tilesData.tiles.pwn}/>
 
             <div className="split-item">
-              <div className="split-item-content center-content-mobile reveal-from-right" data-reveal-container=".split-item">
+              <div className="split-item-content center-content-mobile reveal-from-right"
+                   data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
                   House of Tracks - May 2020 until April 2021
-                  </div>
+                </div>
                 <h3 className="mt-0 mb-12">
                   React based e-commerce
-                  </h3>
+                </h3>
                 <p className="m-0">
-                  The music marketplace <a href={"https://www.houseoftracks.com/"}>House Of Tracks</a> launched a new website where I was responsible for
+                  The music marketplace <a href={"https://www.houseoftracks.com/"}>House Of Tracks</a> launched a new
+                  website where I was responsible for
                   building new features, optimizing the mobile version and maintaining the website.
                   I used React and JavaScript to create dynamic features in
                   the website. In addition to the House Of Tracks website,
@@ -156,16 +196,16 @@ const FeaturesSplitWork = ({
               </div>
               <div className={
                 classNames(
-                  'split-item-image center-content-mobile reveal-from-bottom has-shadow',
-                  imageFill && 'split-item-image-fill'
+                    'split-item-image center-content-mobile reveal-from-bottom has-shadow',
+                    imageFill && 'split-item-image-fill'
                 )}
-                data-reveal-container=".split-item">
+                   data-reveal-container=".split-item">
                 <a href={"https://www.houseoftracks.com"} target={"_blank"} rel={"noreferrer noopener"}>
-                <Image
-                  src={require('./../../assets/images/houseoftracks.png')}
-                  alt="House Of Tracks"
-                  width={528}
-                  height={396} />
+                  <Image
+                      src={require('./../../assets/images/houseoftracks.png')}
+                      alt="House Of Tracks"
+                      width={528}
+                      height={396}/>
                 </a>
               </div>
             </div>
